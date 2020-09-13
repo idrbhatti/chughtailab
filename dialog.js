7b0fc9fa4a90820819ad85810e30811c8e266a8d
Hassan Bhatti <i.drbhatti@gmail.com>
2020-09-13 18:30:50 +0500

    init

---
+++ dialog.js
@@ -0,0 +1,288 @@
+var Dialog = {
+    "version": "2.2",
+    "FormId": "https://docs.google.com/forms/d/e/1FAIpQLSftbAIjwvB1Nm9E4_SZH7Le7tEKZNVEqDC5hwMXeT_Gb4-0UA/viewform?usp=embed_facebook",
+    "messages": [
+        {
+            "message": {
+                "text": [
+                    "Hi",
+                    "Welcome to Test 1"
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Example for pre and post test by Hassan Ashfaq."
+                ]
+            }
+        },
+        {
+            "message": {
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Get Started",
+                        "payload": "Get Started",
+                        "edit": false
+                    }
+                ],
+                "text": [
+                    "Ready for a few questions?"
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    ""
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": "emailAddress",
+                "text": [
+                    "Email address"
+                ],
+                "input": {
+                    "type": "email",
+                    "placeholder": "Your answer...",
+                    "validation": "Type an email address"
+                }
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    ""
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 249698991,
+                "text": [
+                    "Student Name "
+                ],
+                "input": {
+                    "type": "text",
+                    "placeholder": "Your answer..."
+                }
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    ":clap::clap::clap:"
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    ""
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 763338,
+                "text": [
+                    "Roll No:"
+                ],
+                "input": {
+                    "type": "text",
+                    "placeholder": "Your answer..."
+                }
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Way to go!"
+                ],
+                "script": "[%messages_1135574923%]"
+            }
+        }
+    ],
+    "messages_1135574923": [
+        {
+            "message": {
+                "text": [
+                    ""
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Questions"
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Halfway through"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 1583202817,
+                "text": [
+                    "Which methods of birth control needs a prescription?"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Birth control pill"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Contraceptive patch"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Cervical cap"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Diaphragm"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "All of the above"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Thank you!"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 2012302290,
+                "text": [
+                    "What do male condoms offer that other forms of birth control do not?"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Least chance of failure"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Best protection against STDs"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Cheapest to use"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "All of the above"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "2 more questions to go"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 933743536,
+                "text": [
+                    "Besides the condom, which is another barrier method of birth control?"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Diaphragm"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "IUD"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Withdrawal"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Sterilization"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 1108786240,
+                "text": [
+                    "Which type of intrauterine device (IUD) is available?"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Copper"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Titanium"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Hormonal"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "A and C"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "All of the above"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    ""
+                ],
+                "script": "[%messages_submit%]"
+            }
+        }
+    ],
+    "messages_submit": [
+        {
+            "message": {
+                "quick_replies": [
+                    {
+                        "content_type": "submit-form",
+                        "title": "Submit",
+                        "submit_url": "https://docs.google.com/forms/d/e/1FAIpQLSftbAIjwvB1Nm9E4_SZH7Le7tEKZNVEqDC5hwMXeT_Gb4-0UA/formResponse",
+                        "payload": ""
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Your test has been submitted, you may close this chat now.\n\nDeveloped by Muhammad Hassan Ashfaq @ https://doctorbhatti.cf/"
+                ],
+                "last": true
+            }
+        }
+    ]
+};
