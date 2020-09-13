88ba32a92227e3b009f3421b2ba0f440fcd3bae4
Hassan Bhatti <i.drbhatti@gmail.com>
2020-09-14 02:38:41 +0500

    test2_deployed_pre

---
+++ dialog.js
@@ -0,0 +1,519 @@
+var Dialog = {
+    "version": "2.2",
+    "FormId": "https://docs.google.com/forms/d/e/1FAIpQLSeHF0JYmRcaZbidtKiwstw5CmThlFqpqreD1l1PAmd6vZPSPg/viewform?usp=embed_facebook",
+    "messages": [
+        {
+            "message": {
+                "text": [
+                    "Hi! I am Rusty the test bot by Muhammad Hassan Ashfaq.",
+                    "Welcome to Pre-Lecture Test of Community Medicine"
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "on 14/09/2020"
+                ]
+            }
+        },
+        {
+            "message": {
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Get Started",
+                        "payload": "Get Started",
+                        "edit": false
+                    }
+                ],
+                "text": [
+                    "Let's start from an introduction by your side."
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Please be HONEST 😸."
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": "emailAddress",
+                "text": [
+                    "Please let me know your eMail address.I promise I won't spam😔."
+                ],
+                "input": {
+                    "type": "email",
+                    "placeholder": "Your answer...",
+                    "validation": "Type an email address"
+                }
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Thanks alot."
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 1673149897,
+                "text": [
+                    "And of course I would love to know your name."
+                ],
+                "input": {
+                    "type": "text",
+                    "placeholder": "Your answer..."
+                }
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Thanks!"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 304845157,
+                "text": [
+                    "Yeah yeah, just for official purposes please enter your roll number."
+                ],
+                "input": {
+                    "type": "text",
+                    "placeholder": "Your answer..."
+                }
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "cheers :clap:"
+                ],
+                "script": "[%messages_1354328994%]"
+            }
+        }
+    ],
+    "messages_1354328994": [
+        {
+            "message": {
+                "text": [
+                    "Great! Now come let show others your great knowledge on currently accessed topic. 😉",
+                    "DISCLAIMER!! Attempt all MCQs, each MCQ carry equal marks and each MCQ is time locked so once you attempt or one minute passed it will move to next."
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Question 1 out of 10"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 1774294645,
+                "text": [
+                    "I was appointed to perform as doctor in a place pack with trees, fresh air, lake, agriculture land full of crops and cattle rearing in the mountains. I love this sort of:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Hospital"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Clinic"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Village"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Environment"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "City"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    ":clap::clap::clap:"
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Question 2 out of 10"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 1485415880,
+                "text": [
+                    "Things like bacteria, Viruses, Chemicals, Stress, Noise that are harmful in environment and have bad effects on your health are called:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Hazards"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Risks"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Vulnerability"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Nature"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Atmosphere"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Next ..."
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 570835691,
+                "text": [
+                    "Healthy environment, good food and exercise helps in maintaining ecological conditions that prevent diseases and promote:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Sports"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Health"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Nutrition"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Gardening"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Life style"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "cheers :clap:"
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "You are doing great, keep it up! :clap:"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 628153659,
+                "text": [
+                    "Anything that is poisonous or dangerous to health is labelled as:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Hazard"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Risk"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Toxic"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Chemical"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Element"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Thank you!"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 496273773,
+                "text": [
+                    "Source, Environmental pathway and contact are the three parts of:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Hazard"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Exposure"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Toxic"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Risk"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Element"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "cheers :clap:"
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "You're halfway through, doing great."
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 630034836,
+                "text": [
+                    "The quantity of poisonous or dangerous thing enters into the body is called:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Hazard"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Toxic"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Exposure"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Dose"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Risk"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Hang in there, just 4 more to go"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 276302841,
+                "text": [
+                    "Exposure to any chemical and dangerous material depends on dose of the chemical and exposure:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Duration"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Risk"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Hazard"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Toxic"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Element"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "3 more questions to go"
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 729814974,
+                "text": [
+                    "Body size, age and life style play a vital role in the individual:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Hazard"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Risk"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Dose"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Exposure"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Susceptibility"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 433868236,
+                "text": [
+                    "Calculated potential benefit keep us healthy by reducing:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Hazard"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Risk"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Toxicity"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Exposure"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Susceptibility"
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Moving on to final question."
+                ]
+            }
+        },
+        {
+            "message": {
+                "payload": 1871554944,
+                "text": [
+                    " Covid-19 dermal absorption route of spreads is through:"
+                ],
+                "quick_replies": [
+                    {
+                        "content_type": "text",
+                        "title": "Hands"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Nose"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Eyes"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Lungs"
+                    },
+                    {
+                        "content_type": "text",
+                        "title": "Intestines"
+                    }
+                ],
+                "script": "[%messages_submit%]"
+            }
+        }
+    ],
+    "messages_submit": [
+        {
+            "message": {
+                "quick_replies": [
+                    {
+                        "content_type": "submit-form",
+                        "title": "Submit",
+                        "submit_url": "https://docs.google.com/forms/d/e/1FAIpQLSeHF0JYmRcaZbidtKiwstw5CmThlFqpqreD1l1PAmd6vZPSPg/formResponse",
+                        "payload": ""
+                    }
+                ]
+            }
+        },
+        {
+            "message": {
+                "text": [
+                    "Thanks alot for bearing with me.Your test has been submitted, you may close this chat now.\n\nThis web app is part of my personal educational suite in development.\nApologies in advance for any bugs or inconvenience.\nDeveloped by Muhammad Hassan Ashfaq @ https://doctorbhatti.cf/\n\nCopyright Ⓒ 2020, Doctor Bhatti Develops."
+                ],
+                "last": true
+            }
+        }
+    ]
+};
